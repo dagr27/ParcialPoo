5bbb7d578dfeb2b74b15ff0486517019566157fe
ErikR <ErikR@192.168.1.57>
2018-05-04 00:20:25 -0600

    Se creo la funcion "AgregarHabitacion" que sera la encargada de agregar una nueva habitacion si el administrador asi lo desea. Tambien se creo la funcion "MostrarHabitacion" que simplemente muestra todas las habitaciones existentes.

--- src/hotelparcialpoo/ListaHabitacion.java
+++ src/hotelparcialpoo/ListaHabitacion.java
@@ -71,4 +71,54 @@ public class ListaHabitacion {
             throw new Exception("No se puede agregar una habitacion nula");
         }
     }
+    
+    public void AgregarHabitacion(){
+        Scanner leer = new Scanner(System.in);
+        int contadorID = 1;
+        int ascii = 65;
+        char letra = ((char)(ascii));
+        int precioBase = 60;
+        Habitacion room = new Habitacion();
+        for(int i=0;i<=rooms.size()+1;i++)
+        {
+           
+                String result = String.valueOf(letra)+String.valueOf(contadorID-1);
+                room.setIdRoom(result);
+                room.setStatus("Disponible");
+                
+                if(contadorID%2==0){
+                    room.setType("Doble");
+                    room.setPrice(rooms.get(0).price+((rooms.get(0).price*15)/100));
+                }else{
+                    room.setType("Sencilla");
+                    room.setPrice(rooms.get(0).price);
+                }
+
+                if(letra==((char)(69)) || letra==(char)(70)){
+                    if(contadorID%2==0){
+                        room.setPrice(rooms.get(0).price+(((rooms.get(0).price*10)+(rooms.get(0).price*15))/100));
+                    }else{
+                        room.setPrice(rooms.get(0).price+((rooms.get(0).price*10)/100));
+                    }
+                }
+                
+                contadorID += 1;
+           
+                if(contadorID>10){
+                    contadorID = 1;
+                    ascii = ascii + 1;
+                    letra = ((char)(ascii));
+                }
+            }
+        rooms.add(room);
+        System.out.println("\033[32mHABITACION AGREGADA CORRECTAMENTE.");
+    }
+    
+     public void MostrarHabitacion(){
+        for(Habitacion habitacion : rooms) {
+            System.out.println(habitacion.idRoom);
+            System.out.println(habitacion.price);
+            System.out.println(habitacion.status);
+        }
+    }
 }
