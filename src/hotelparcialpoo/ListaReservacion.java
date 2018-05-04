8ab08296cef851f1eabf93c6ba313b44cbe71701
Daniel Gómez <37205358+dagr27@users.noreply.github.com>
2018-05-03 23:32:01 -0600

    Creando clase de Lisra Reservacion

---
+++ src/hotelparcialpoo/ListaReservacion.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelparcialpoo;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+/**
+ *
+ * @author Daniel Gómez
+ */
+public class ListaReservacion {
+    private ArrayList<Reservacion> reservations;
+    public ListaReservacion(){
+        reservations = new ArrayList<>();
+    }
+}
