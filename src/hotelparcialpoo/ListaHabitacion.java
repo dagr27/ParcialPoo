b640926e9d137fae025528478be20d4ed67c8853
ErikR <ErikR@192.168.1.57>
2018-05-04 00:16:42 -0600

    Se crea la clase "ListaHabitacion", que contendra las funciones para agregar, modificar, ver y buscar habitaciones.

---
+++ src/hotelparcialpoo/ListaHabitacion.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelparcialpoo;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+/**
+ *
+ * @author ErikR
+ */
+public class ListaHabitacion {
+    private ArrayList<Habitacion> rooms;
+    public ListaHabitacion(){
+        rooms = new ArrayList<>();
+    }
+    int nHabitacion = 59;
+    
+    
+}
