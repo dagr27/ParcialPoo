592dbec1ba85a196e5307fc4f92945505939d572
Brenda <Brenda@192.168.0.22>
2018-05-03 22:38:01 -0600

    Creando clase lista usuarios

---
+++ src/hotelparcialpoo/ListaUsuarios.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelparcialpoo;
+import java.util.ArrayList;
+import java.util.Scanner;
+/**
+ * @author ErikR
+ * @author DanielG
+ * @author EstherL
+ * @author Brenda
+ */
+public class ListaUsuarios {
+     private ArrayList<Usuario>users;
+    public ListaUsuarios(){
+        users = new ArrayList<>();
+}
+}
