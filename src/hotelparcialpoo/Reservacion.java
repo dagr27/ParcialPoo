abf837dcd21cdaf5276225b13a92c4f1b1970b5a
Daniel Gómez <37205358+dagr27@users.noreply.github.com>
2018-05-03 23:25:28 -0600

    Creando clase reservacion

---
+++ src/hotelparcialpoo/Reservacion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelparcialpoo;
+
+/**
+ *
+ * @author Daniel Gómez
+ */
+public class Reservacion {
+    
+}
