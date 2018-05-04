e01360c57939e142bb317d6e12c43fcf4f593399
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2018-05-04 02:16:16 -0600

    agregando submenu

--- src/hotelparcialpoo/Menu.java
+++ src/hotelparcialpoo/Menu.java
@@ -81,6 +81,99 @@ public class Menu {
             }
         }
             }
+    
+    public void mostrarSubmenu(){
+        Scanner leer = new Scanner(System.in);
+        listaH.add();
+        lista2.add();
+        int opcion = 0;
+        int estado,op,precioNuevo,paquete;
+        String piso,habitacion, servicio;
+        System.out.println("\033[40m----- Menu Habitaciones -----");
+        System.out.println("1. Mostrar todas las Habitaciones");
+        System.out.println("2. Agregar habitacion");
+        System.out.println("3. Habilitar/Deshabilitar habitacion/piso");
+        System.out.println("4. Cambiar precio base de las habitaciones");
+        System.out.println("5. Precio y servicio de los paquetes");
+        System.out.println("6. Regresar");
+            try {
+                opcion = leer.nextInt();
+                switch (opcion) {
+                    case 1:
+                        listaH.MostrarHabitacion();
+                        break;
+                        
+                     case 2:
+                        System.out.println("¿Seguro que deseas agregar una nueva habitación?\n1. Si\n2. No");
+                        op = leer.nextInt();
+                        leer.nextLine();
+                        if(op==1){
+                            listaH.AgregarHabitacion();
+                        }
+                        break;
+                        
+                    case 3:
+                        System.out.println("Desea cambiar estado de:\n1. Piso\n2. Habitacion");
+                        op = leer.nextInt();
+                        leer.nextLine();
+                        if(op==1){
+                            System.out.println("Ingrese el piso:");
+                            piso = leer.nextLine();
+                            System.out.println("Seleccione una opcion:\n1. Habilitar\n2. Deshabilitar");
+                            estado = leer.nextInt();
+                            listaH.CambiarEstadoPiso(piso, estado);
+                            
+                        }else{
+                            System.out.println("Ingrese la habitacion:");
+                            habitacion = leer.nextLine();
+                            System.out.println("Seleccione una opcion:\n1. Habilitar\n2. Deshabilitar");
+                            estado = leer.nextInt();
+                            listaH.CambiarEstado(habitacion, estado);
+                        }
+                        break;
+                        
+                    case 4:
+
+                        System.out.println("Ingrese el nuevo precio base:");
+                        precioNuevo = leer.nextInt();
+                        leer.nextLine();
+                        listaH.CambiarPrecio(precioNuevo);
+                        
+                        break;
+                        
+                    case 5:
+                        System.out.println("Desea cambiar:\n1. Precio\n2. Servicios");
+                        op = leer.nextInt();
+                        leer.nextLine();
+                        if(op==1){
+                            System.out.println("PAQUETE:\n1. PREMIUM\n2. BASICO");
+                            paquete = leer.nextInt();
+                            leer.nextLine();
+                            System.out.println("Ingrese el nuevo precio:");
+                            precioNuevo = leer.nextInt();
+                            lista2.CambiarPrecio(paquete, precioNuevo);
+                        }else{
+                            System.out.println("PAQUETE:\n1. PREMIUM\n2. BASICO");
+                            paquete = leer.nextInt();
+                            leer.nextLine();
+                            System.out.println("Ingrese el nuevo servicio a ofrecer:");
+                            servicio = leer.nextLine();
+                            lista2.CambiarServicio(paquete, servicio);
+                        }
+                        break;
+                    case 6:
+                        break;
+                    default:
+                        System.out.println("Por favor ingrese una opcion valida.");
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                System.err.println("Por favor, vuelva a intentarlo y revise los datos ingresados.");
+                
+            }
+        }
+    
+    
     }
         
 
